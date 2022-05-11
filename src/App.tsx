c68688fea63279b79e6ef46877d556e91cee40a0
Ronald Arias <ariasron@hotmail.com>
2022-05-11 17:21:55 -0500

    installs groomet component library and adds fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import './App.css'
+import { Grommet, Heading } from 'grommet'
+import theme from './grommet-styles/global-styles'
 
 const App: React.FC = (): JSX.Element => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Hello World</h1>
+  <Grommet theme={theme}>
+    <header>
+      <Heading level="1">Aliquam porttitor mauris sit amet orci Aenean</Heading>
+      <Heading level="2">Aliquam porttitor mauris sit amet orci Aenean</Heading>
+      <Heading level="3">Aliquam porttitor mauris sit amet orci Aenean</Heading>
+      <Heading level="4">Aliquam porttitor mauris sit amet orci Aenean</Heading>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque
+        aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
+        pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc
+        tortor eu nibh. Nullam mollis. Ut justo. Suspendisse potenti.
+      </p>
     </header>
-  </div>
+  </Grommet>
 )
 
 export default App
---
+++ src/grommet-styles/global-styles.ts
@@ -0,0 +1,41 @@
+const theme = {
+  global: {
+    font: {
+      family: 'League Spartan',
+      size: '12px',
+      height: '15px',
+      weight: '500'
+    }
+  },
+  heading: {
+    level: {
+      1: {
+        medium: {
+          size: '32px',
+          height: '36px'
+        }
+      },
+      2: {
+        medium: {
+          size: '20px',
+          height: '22px'
+        }
+      },
+      3: {
+        medium: {
+          size: '16px',
+          height: '24px'
+        }
+      },
+      4: {
+        medium: {
+          size: '12px',
+          height: '15px'
+        }
+      }
+    },
+    weight: 700
+  }
+}
+
+export default theme
