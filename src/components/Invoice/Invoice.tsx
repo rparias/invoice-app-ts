f4ce45988926b5377b5bbc1323ae16750aa933e6
Ronald Arias <ariasron@hotmail.com>
2022-05-19 09:13:08 -0500

    initial grid for mobile and desktop version on Invoice component

--- src/components/Invoice/Invoice.tsx
+++ src/components/Invoice/Invoice.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import { Tag, Text } from 'grommet'
+import { InvoiceContainer } from './styles'
 
-const Invoice: React.FC = (): JSX.Element => <div>Invoice</div>
+const Invoice: React.FC = (): JSX.Element => (
+  <InvoiceContainer>
+    <Text a11yTitle="Invoice Id" gridArea="id">
+      #RT3080
+    </Text>
+    <Text a11yTitle="Invoice date" gridArea="date">
+      Due 19 Aug 2021
+    </Text>
+    <Text a11yTitle="Invoice name" gridArea="name">
+      Jensen Huang
+    </Text>
+    <Text a11yTitle="Invoice price" gridArea="price">
+      $1,800.90
+    </Text>
+    <Tag a11yTitle="Invoice status" gridArea="status" value="paid" />
+  </InvoiceContainer>
+)
 
 export default Invoice
---
+++ src/components/Invoice/styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const InvoiceContainer = styled.article`
+  border: 1px solid red;
+  width: 80%;
+  display: grid;
+  grid-template-areas: 'id date name price status';
+
+  @media only screen and (max-width: 425px) {
+    grid-template-areas:
+      'id name'
+      'date status'
+      'price status';
+  }
+`
+
+export { InvoiceContainer }
