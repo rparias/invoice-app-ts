fde20e7635b38af68195ea6b231f1059936f9d09
Ronald Arias <ariasron@hotmail.com>
2022-06-08 11:15:02 -0500

    starts creating the empty page component

---
+++ src/layout/EmptyPage/EmptyPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box, Heading, Text } from 'grommet'
+import { ReactComponent as EmptyIllustration } from '../../assets/illustration-empty.svg'
+
+const EmptyPage: React.FC = (): JSX.Element => (
+  <Box align="center" justify="center" height="80%">
+    <EmptyIllustration style={{ marginBottom: '3rem' }} />
+    <Heading level={2}>There is nothing here</Heading>
+    <Text color="dark-4">Create an invoice by clicking the</Text>
+    <Text color="dark-4">
+      <span style={{ fontWeight: 'bold' }}>New Invoice</span> button and get started
+    </Text>
+  </Box>
+)
+
+export default EmptyPage
