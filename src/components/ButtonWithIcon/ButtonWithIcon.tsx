2db69f7c6101c6f6d61ecfb0d2c13258610d0e60
Ronald Arias <ariasron@hotmail.com>
2022-08-05 15:29:11 -0500

    updates buttonWithIcon component to support type and onclick

--- src/components/ButtonWithIcon/ButtonWithIcon.tsx
+++ src/components/ButtonWithIcon/ButtonWithIcon.tsx
@@ -4,13 +4,20 @@ import { ReactComponent as PlusIcon } from '../../assets/icon-plus.svg'
 
 type Props = {
   label: string
+  type?: 'button' | 'submit' | 'reset' | undefined
+  onClick?: () => void
 }
 
-const ButtonWithIcon: React.FC<Props> = ({ label }): JSX.Element => (
-  <ButtonWithIconContainer>
+const ButtonWithIcon: React.FC<Props> = ({ label, type, onClick }): JSX.Element => (
+  <ButtonWithIconContainer type={type} onClick={onClick}>
     <PlusIcon className="icon" />
     {label}
   </ButtonWithIconContainer>
 )
+
+ButtonWithIcon.defaultProps = {
+  type: 'button',
+  onClick: () => null
+}
 
 export default ButtonWithIcon
