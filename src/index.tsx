9b18aa88f9ed03be7abce35ab43311c57b3927cc
Ronald Arias <ariasron@hotmail.com>
2022-08-05 15:40:40 -0500

    adds a provider in the index file to support the store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux'
 import App from './App'
+import { setupStore } from './state/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const store = setupStore()
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
